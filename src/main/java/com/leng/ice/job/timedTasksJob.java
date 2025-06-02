eebe9ec572bed91edf0fa1d9826b3201db57ba8e
xiaoleng <1873048956@qq.com>
2025-06-02 19:33:45 +0800

    feat:使用Spring Scheduler定时任务来实现缓存预热

--- src/main/java/com/leng/ice/job/timedTasksJob.java
+++ src/main/java/com/leng/ice/job/timedTasksJob.java
@@ -2,16 +2,24 @@ package com.leng.ice.job;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.leng.ice.common.ErrorCode;
+import com.leng.ice.contant.RedisConstant;
 import com.leng.ice.exception.BusinessException;
 import com.leng.ice.model.entity.Team;
+import com.leng.ice.model.entity.User;
 import com.leng.ice.service.TeamService;
+import com.leng.ice.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 每天凌晨0点解散过期队伍
@@ -24,6 +32,15 @@ public class timedTasksJob {
 
     @Resource
     private TeamService teamService;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 每天0点执行解散过期队伍
@@ -45,39 +62,40 @@ public class timedTasksJob {
         });
     }
 
-//    // 每天每小时更新一次点执行，更新用户列表，集群部署下只需要一个服务器完成缓存更新
-//    @Scheduled(cron = "0 0 0-23 * * ?")
-//    public void updateUserList() {
-//        //获取锁
-//        RLock lock = redissonClient.getLock("ice:precachejob:docache:lock");
-//        try {
-//            if (lock.tryLock(0,-1, TimeUnit.MILLISECONDS)){
-//                //当前获得锁的线程的id是
-//                System.out.println("getLock"+Thread.currentThread().getId());
-//                //查询信息
-//
-//                QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-//                //用户脱敏
-//                queryWrapper.select("id", "username", "userAccount"
-//                        , "userProfile", "avatarUrl", "gender", "phone"
-//                        , "email", "tags", "userRole", "updateTime", "createTime", "userState");
-//                List<User> userPage = userService.list(queryWrapper);
-//                String redisKey = REDIS_RECOMMEND_KEY;
-//                ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-//                //写缓存
-//                try {
-//                    valueOperations.set(redisKey,userPage);
-//                } catch (Exception e) {
-//                    log.error("redis set key error",e);
-//                }
-//            }
-//        } catch (InterruptedException e) {
-//            log.error("doCacheRecommendUser error", e);
-//        }finally {
-//            //只能自己释放自己的锁
-//            if (lock.isHeldByCurrentThread()){
-//                lock.unlock();
-//            }
-//        }
-//    }
+    /**
+     * 缓存预热，每天每小时更新一次点执行，更新用户列表，集群部署下只需要一个服务器完成缓存更新
+     */
+    @Scheduled(cron = "0 0 0-23 * * ?")
+    public void updateUserList() {
+        //获取锁
+        RLock lock = redissonClient.getLock("ice:precachejob:docache:lock");
+        try {
+            if (lock.tryLock(0,-1, TimeUnit.MILLISECONDS)){
+                //当前获得锁的线程的id是
+                System.out.println("getLock"+Thread.currentThread().getId());
+                //查询信息
+                QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                //用户脱敏
+                queryWrapper.select("id", "username", "userAccount"
+                        , "userProfile", "avatarUrl", "gender", "phone"
+                        , "email", "tags", "userRole", "updateTime", "createTime", "userState");
+                List<User> userPage = userService.list(queryWrapper);
+                String redisKey = RedisConstant.REDIS_RECOMMEND_KEY;
+                ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                //写缓存
+                try {
+                    valueOperations.set(redisKey,userPage);
+                } catch (Exception e) {
+                    log.error("redis set key error",e);
+                }
+            }
+        } catch (InterruptedException e) {
+            log.error("doCacheRecommendUser error", e);
+        }finally {
+            //只能自己释放自己的锁
+            if (lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
+        }
+    }
 }
